291b82d9c5131c8658e625a76612e93e9806dff1
David Pratt <david@midnightmechanism.com>
2023-02-14 22:20:41 -0800

    Print out correspondence between pkgs and data

---
+++ package-mine/scripts/tables.R
@@ -0,0 +1,47 @@
+#!/usr/bin/env Rscript 
+
+pkg_list = list.files(path = "../uncompressed-R-packages")
+
+# print(pkg_list)
+
+# example_idx <- which(pkg_list == "MASS")
+# print(example_idx)
+
+# example_idx = 17 # a different example
+
+package_data = list()
+
+for(p in 1:length(pkg_list)){
+    
+    # print names of data files associated with each package
+    data_files <- list.files(path = paste0("../uncompressed-R-packages/", pkg_list[[p]], "/data")) # example_idx
+    data_files <- sub(".rda", "", c(data_files) ) 
+    data_files <- sub(".RData", "", c(data_files) )
+    data_files <- sub(".txt.gz", "", c(data_files) )
+    data_files <- sub(".csv", "", c(data_files) )
+    data_files <- sub(".tab.gz", "", c(data_files) )
+    
+    # print( length(data_files) ) # how many data sets exist in particular pkg?
+    reps = length(data_files)
+
+    # stop packages with no data from being deleted
+    if(reps == 0){
+        reps = 1
+        data_files = "null"
+    } 
+
+    # create relationship between package and data sets
+    package_data[[p]] = cbind.data.frame( rep(pkg_list[[p]], reps),  data_files ) # example_idx
+    print(p)
+}
+
+# Put all packages together 
+all_data = do.call(rbind, package_data)
+colnames(all_data) = c("package", "data_set")
+
+# write csv for inspection purposes
+write.csv(all_data, "all_data.csv", row.names = FALSE)
+
+# measure length of unique values
+length(unique(all_data$package)) # only about 8,000 or so of these have associated data
+length(unique(all_data$data_set))
