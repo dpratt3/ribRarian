22f4b10dee5cd82edca91fe5810e1269ca35bbb4
David Pratt <david@midnightmechanism.com>
2023-02-14 19:21:08 -0800

    Make package scraper executable

--- package-mine/scripts/package-scraper.R
+++ package-mine/scripts/package-scraper.R
@@ -1,3 +1,4 @@
+#!/usr/bin/env Rscript 
 # https://stackoverflow.com/questions/39051381/how-to-download-the-entire-cran-repository
 
 pkgnames <- available.packages()[,1]
@@ -9,19 +10,4 @@ tab <- html_nodes(pkgs, "table") %>% html_table(fill = TRUE)
 pkgnames <- tab[[1]][1]$X1
 pkgnames <- pkgnames[nchar(pkgnames)>0]
 
-sapply(pkgnames, download.packages, destdir = "/home/david/git/app-academy/capstone/R-packages")
-
-# sapply(pkgnames, install.packages, dependencies = TRUE,  destdir = "/home/david/git/app-academy/capstone/R-packages")
-# for(i in 1: length(pkgnames)}{
-#     install.packages(pkgnames[i], dependencies = TRUE)
-# }
-
-### Datasets available in all packages:
-# https://stats.oarc.ucla.edu/r/faq/how-can-i-see-what-data-sets-are-available-when-i-start-r/
-# data(package = .packages(all.available = TRUE))
-
-# Data in a particular package
-# data(package = "A3")
-
-# R packages that share data: https://r-pkgs.org/data.html
-
+sapply(pkgnames, download.packages, destdir = "~/git/app-academy/ribRarian/package-mine/compressed-R-packages")
