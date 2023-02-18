5edc9d6123caaf83dfdab057401e253fb6406cf1
David Pratt <david@midnightmechanism.com>
2023-02-18 13:23:09 -0800

    Update db helper

--- flask-app/db-helper.sh
+++ flask-app/db-helper.sh
@@ -3,9 +3,10 @@
 # to use, run `chmod + x ./db-helper.sh`
 # followed by ./db-helper.sh
 
-rm -r migrations
-rm app/dev.db
+# rm -r migrations
+rm ./instance/dev.db
 
 flask db init
 flask db migrate
 flask db upgrade
+flask seed all
