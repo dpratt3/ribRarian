8f548388f437fea05f634ed033a65245882cd9d8
David Pratt <david@midnightmechanism.com>
2023-02-19 15:51:28 -0800

    Stamp head to address migration error and remove migrations

--- flask-app/db-helper.sh
+++ flask-app/db-helper.sh
@@ -3,10 +3,11 @@
 # to use, run `chmod + x ./db-helper.sh`
 # followed by ./db-helper.sh
 
-# rm -r migrations
+rm -r seeddata1/migrations
 rm ./instance/dev.db
 
 flask db init
+flask db stamp head # fix error: https://stackoverflow.com/questions/17768940/target-database-is-not-up-to-date
 flask db migrate
 flask db upgrade
 flask seed all
