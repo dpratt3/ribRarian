c1f6787e630b69c8544d72f562a8d0648f445836
David Pratt <david@midnightmechanism.com>
2023-02-20 20:27:00 -0800

    Complete 'Setting up a Schema in Flask from week 41'

--- flask-app/app/models/ribRarian.py
+++ flask-app/app/models/ribRarian.py
@@ -1,14 +1,20 @@
-from .db import db, environment, SCHEMA
+from .db import db, environment, SCHEMA, add_prefix_for_prod
 from .db import add_prefix_for_prod
 
 # start with joins table
 
-
 class DatasetPackage(db.Model):
+    
+    __tablename__ = 'dataset_package'
+
+    if environment == "production":
+        __table_args__ = (db.UniqueConstraint(
+            'question_id', 'tag_id'), {'schema': SCHEMA})
+    
     id = db.Column(db.Integer, primary_key=True)
-    user_id = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=False)
-    pkg_key = db.Column(db.Integer, db.ForeignKey('package.id'), nullable=False)
-    data_key = db.Column(db.Integer, db.ForeignKey('dataset.id'), nullable=False)
+    user_id = db.Column(db.Integer, db.ForeignKey(add_prefix_for_prod('users.id')), nullable=False)
+    pkg_key = db.Column(db.Integer, db.ForeignKey(add_prefix_for_prod('package.id')), nullable=False)
+    data_key = db.Column(db.Integer, db.ForeignKey(add_prefix_for_prod('dataset.id')), nullable=False)
     table_name = db.Column(db.String(50))
     
     user = db.relationship('User', backref=db.backref('datasetpackage', lazy=True))
@@ -25,6 +31,13 @@ class DatasetPackage(db.Model):
         }
 
 class Package(db.Model):
+    
+    __tablename__ = 'package'
+
+    if environment == "production":
+        __table_args__ = (db.UniqueConstraint(
+            'question_id', 'tag_id'), {'schema': SCHEMA})
+        
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(255))
     links = db.Column(db.String(255))
@@ -39,6 +52,12 @@ class Package(db.Model):
         }
     
 class Dataset(db.Model):
+    __tablename__ = 'dataset'
+
+    if environment == "production":
+        __table_args__ = (db.UniqueConstraint(
+            'question_id', 'tag_id'), {'schema': SCHEMA})
+        
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(50))
     date_modified = db.Column(db.String(20))    
