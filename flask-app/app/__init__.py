5ebda00205fcc0512e578bf0ad8fabcc530ed898
David Pratt <david@midnightmechanism.com>
2023-02-20 21:40:39 -0800

    Comment out lines 94-95

--- flask-app/app/__init__.py
+++ flask-app/app/__init__.py
@@ -91,8 +91,8 @@ def not_found(e):
     return app.send_static_file('index.html')
 
 # bottom of file
-@app.route('/', defaults={'path': ''})
-@app.route('/<path:path>')
+# @app.route('/', defaults={'path': ''})
+# @app.route('/<path:path>')
 def react_root(path):
     if path == 'favicon.ico':
         return app.send_from_directory('public', 'favicon.ico')
